84d0d22d1ff2d81a74f97fc111408140f75bc8bb
vivian xu <vivianwxu0@gmail.com>
2024-12-08 18:52:04 -0500

    set up gh-pages

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom/client";
 import Mapper from "./components/Mapper";
 import day from './../images/city-day-v0.png';
 import night from './../images/city-night-v0.png';
-import areas from './map-areas.json';
+import areas from './../map-areas.json';
 
 function App() {
     
