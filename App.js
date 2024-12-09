3e5f1ef24960a90e9666ac451680d0a2d2bcee02
vivian xu <vivianwxu0@gmail.com>
2024-12-08 21:54:21 -0500

    relocated App to separate file from index.js

--- App.js
+++ App.js
@@ -1,11 +1,10 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import Mapper from "./components/Mapper";
 import day from './images/city-day-v0.png';
 import night from './images/city-night-v0.png';
 import areas from './map-areas.json';
 
-function App() {
+export function App() {
     
     const [mode, setMode] = React.useState("day");
 
@@ -23,11 +22,3 @@ function App() {
         </div>
     );
 };
-
-
- 
- // add click event listener
- 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <App/>
-);
---
+++ index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import { App } from "./App";
+ 
+ // add click event listener
+ 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <App/>
+);
